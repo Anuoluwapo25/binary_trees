07bd6238b6ec2faaf76a116ac7af1806b9f394c8
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2024-02-01 10:01:14 -0800

    delete

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,6 +1,7 @@
 #include "binary_trees.h"
-/*
- * binary_tree_delete - function that inserts a node as the left-child of another node
+
+/**
+ * binary_tree_delete - function that inserts a node as the left-child
  * @tree: the tree
  * Return: void
  */
