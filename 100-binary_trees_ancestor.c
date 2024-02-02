bc86be16aa54dc2cd5a6d7deac22e3530a208fa5
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2024-02-01 17:37:31 -0800

    adv

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -9,4 +9,29 @@
 
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
+	binary_tree_t *temp;
+	binary_tree_t *temp1;
+
+	if (first == NULL || second == NULL)
+		return (NULL);
+	if (first->parent == second->parent)
+		return (first->parent);
+
+	if (first == second)
+		return ((binary_tree_t *)first->parent);
+
+	temp = (binary_tree_t *)first;
+	while (temp != NULL)
+	{
+		temp1 = (binary_tree_t *)second;
+		while (temp1 != NULL)
+		{
+			if (temp == temp1)
+				return (temp);
+			temp1 = temp1->parent;
+		}
+		temp = temp->parent;
+	}
+	return (NULL);
+}
 
